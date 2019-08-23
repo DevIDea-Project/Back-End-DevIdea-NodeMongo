397d91814dd98c8f0f50f4551cb0292d60e47e75
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-08-23 13:40:21 -0300

    Commit: Validando se tem usuario.

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -5,8 +5,17 @@ const User = require('../models/users');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+ 
+  const { email } = req.body;
+
   try {
+    if(await User.findOne({ email }))
+      return res.status(400).send({ error: 'Usuario já existe!'});
+
     const user = await User.create(req.body);
+
+    user.password = undefined;
+    
     return res.send({ user })
   } 
   catch (err) {

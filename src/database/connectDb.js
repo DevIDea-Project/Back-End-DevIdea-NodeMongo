47445dac51d0eebc8aec22cbf30bd19637d907f9
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-08-26 10:55:33 -0300

    Commit: Refatorando a logica da conexão.

--- src/database/connectDb.js
+++ src/database/connectDb.js
@@ -4,7 +4,14 @@ mongoose.set('useCreateIndex', true);
 let caminho = 'mongodb://localhost:27017/PortifolioBruno';
 
 mongoose.connect(caminho, {  useNewUrlParser: true });
-mongoose.Promise = global.Promise;
+if (mongoose.connect){
+  mongoose.Promise = global.Promise;
+  console.log('MongoDB já está no Ar.');
+}
+else{ 
+  console.log('Deu pau!');
+}
+
 
 module.exports = mongoose;
     

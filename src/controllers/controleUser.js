b3d2f1e904c51e6b9539f31341b87b9684c4bf29
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-08-30 09:38:53 -0300

    Commit: Atualizado.

--- src/controllers/controleUser.js
+++ src/controllers/controleUser.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.use(authControle);
 
 router.get('/', (req, res) => {
-  res.send({ Message: 'Validação de Token, Ok!' });
+  res.send({ Message: 'Validação de Token, Ok!', user: req.userId });
 })
 
 module.exports = app => app.use('/controle', router);

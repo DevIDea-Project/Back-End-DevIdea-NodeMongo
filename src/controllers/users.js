0d448ae499e0109188dcd5cfda0c094821b94997
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-09-04 17:15:51 -0300

    Commit: Reset password

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -91,9 +91,37 @@ router.post('/forgot_password', async (req, res) => {
     });
 
   } catch (err) {
-     console.log(err);
     res.status(400).send({ error: 'Error on forgot password, try again!' })
   }
+});
+
+router.post('/reset_password', async (req, res) => {
+  const { email , token, password } = req.body;
+
+  try{
+    const user = await User.findOne({ email })
+      .select('+passwordResetToken passwordResetExpires');
+
+      if (!user) return res.status(400).send({ error: 'Usuario não existe!' });
+
+      if(token !== user.passwordResetToken)
+      return res.status(400).send({error: 'Token invalid'});
+
+      const now = new Date();
+
+      if(now > user.passwordResetExpires)
+      res.status(400).send({error: 'Token experid, generate a new one'});
+
+      user.password = password;
+
+      await user.save();
+
+      res.send();
+
+    } catch(err) {
+    res.status(400).send({error: 'Cannot reset password, try again!'})
+  }
+
 });
 
 module.exports = app => app.use('/auth', router);

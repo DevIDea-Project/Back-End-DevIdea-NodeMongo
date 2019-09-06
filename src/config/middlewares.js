5ba5cf08269aa1f45ae2871198a185925c494719
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-09-06 11:27:18 -0300

    Commit: Refatorando.

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,5 +1,8 @@
 const bodyParser = require('body-parser');
 
+/** Aqui fiz o uso do bodyparser justamente para auxiliar na requisições com os verbos
+ * e retornar o body organizado.
+ */
 module.exports = (app) => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }))

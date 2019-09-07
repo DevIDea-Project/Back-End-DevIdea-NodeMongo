0d7c2b5d66f76de5ab7b0543eb600cbd1485ac22
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-09-07 11:24:14 -0300

    Commit: Controler projeto.

---
+++ src/controllers/project.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const authMiddleware = require('../middleware/auth');
+
+//const projects = require('../models/project');
+//const Task = require('../models/task');
+
+const router = express.Router();
+
+router.use(authMiddleware);
+
+router.get('/', async (req, res) => {
+  res.send({ user: req.userId });
+});
+
+router.get('/:projectId', async (req, res) => {
+  res.send({ user: req.userId });
+});
+
+router.post('/', async (req, res) => {
+  res.send({ user: req.userId });
+});
+
+router.put('/projectId', async (req, res) =>{
+  res.send({ user: req.userId });
+});
+
+router.delete('/projectId', async (req, res) => {
+  res.send({ user: req.userId });
+});
+
+module.exports = (app) => app.use('/projects', router);

03876ba9aabc1d206cb4efe08844f632617c0bbc
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-09-12 10:57:13 -0300

    Commit: Projects model. Refatorando.

--- src/models/project.js
+++ src/models/project.js
@@ -1,17 +1,33 @@
 const mongoose = require('../models/connectDb');
 
 const ProjectSchema = new mongoose.Schema({
-  title: { type: String, require: true, },
+  title: { 
+    type: String, 
+    require: true, 
+  },
 
-  description: { type: String, require: true },
+  description: { 
+    type: String, 
+    require: true, 
+  },
 
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true, },
+  user: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    require: true, 
+  },
 
-  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task', require: true, }],
+  tasks: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Task', 
+  }],
 
-  createdAt: { type: Date, dafault: Date.now, }
+  createdAt: { 
+    type: Date, 
+    dafault: Date.now, 
+  },
 });
 
-const Project = mongoose.model('project', ProjectSchema);
+const Project = mongoose.model('Project', ProjectSchema);
 
 module.exports = Project;

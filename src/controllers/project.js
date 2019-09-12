32e3613dd7c81cd08fd56a2eef107248f7403ffa
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-09-12 10:55:39 -0300

    Commit: Implementando CRUD Controller

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -1,38 +1,45 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth');
 
-const Projects = require('../models/project');
+const Project = require('../models/project');
 //const Task = require('../models/task');
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get('/projects', async (req, res) => {
+router.get('/', async (req, res) => {
   
   try{
-    const project = await Projects.find();
-    return res.send({ project });
+    const projects = await Project.find().populate('user');
+    
+    return res.send({ projects });
   
   }catch(err){
     return res.status(400).send({error:'Não listou os projetos.'})
   }
-  //res.send({ user: req.userId });
+
 });
 
 router.get('/:projectId', async (req, res) => {
-  res.send({ user: req.userId });
+  try {
+    const projects = await Project.findById(req.params.projectId).populate('user');
+
+    return res.send({ projects });
+
+  } catch (err) {
+    return res.status(400).send({ error: 'Não listou os projetos.' });
+  }
 });
 
 router.post('/', async (req, res) => {
   try{
-    const project = await Projects.create({ ...req.body, user: req.userId, title: req.title, description: req.description });
+    const projects = await Project.create({ ...req.body, user: req.userId });
 
-    return res.send({ project });
+    return res.send({ projects });
   } catch(err){
     return res.status(400).send({ error: 'Aconteceu algo de errado no envio' })
   }
-  //res.send({ user: req.userId });
 });
 
 router.put('/projectId', async (req, res) =>{
